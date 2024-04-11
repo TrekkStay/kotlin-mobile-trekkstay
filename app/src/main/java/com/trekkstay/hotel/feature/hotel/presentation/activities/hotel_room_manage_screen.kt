75b5e8fd41413f3375866863b6b943c824d8ddb7
thanhanphan17 <thanhanphan17@gmail.com>
2024-04-11 23:22:14 +0700

    feat: hotel room list for hotel management

--- app/src/main/java/com/trekkstay/hotel/feature/hotel/presentation/activities/hotel_room_manage_screen.kt
+++ app/src/main/java/com/trekkstay/hotel/feature/hotel/presentation/activities/hotel_room_manage_screen.kt
@@ -1,11 +1,20 @@
 package com.trekkstay.hotel.feature.hotel.presentation.activities
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.Add
@@ -17,20 +26,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.example.hotel.R
+import com.trekkstay.hotel.feature.hotel.presentation.fragments.HotelRoomCard
 import com.trekkstay.hotel.ui.theme.PoppinsFontFamily
 import com.trekkstay.hotel.ui.theme.TrekkStayBlue
+import com.trekkstay.hotel.ui.theme.TrekkStayCyan
 
 @Composable
 fun HotelRoomManageScreen(navController: NavHostController) {
-    Scaffold (
+    Scaffold(
         modifier = Modifier.padding(bottom = 70.dp),
         floatingActionButton = {
             FloatingActionButton(
@@ -48,7 +68,7 @@ fun HotelRoomManageScreen(navController: NavHostController) {
                 Icon(Icons.Filled.Add, "Floating action button.")
             }
         }
-    ) {_->
+    ) { _ ->
         Column {
             Spacer(modifier = Modifier.height(20.dp))
             Row(
@@ -57,13 +77,36 @@ fun HotelRoomManageScreen(navController: NavHostController) {
                 IconButton(onClick = {
                     navController.navigate("hotel_profile")
                 }) {
-                    Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "backFromRoomManage")
+                    Icon(
+                        Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                        contentDescription = "backFromRoomManage"
+                    )
                 }
-                Text(text = "Your hotel rooms",fontFamily = PoppinsFontFamily, fontWeight = FontWeight.SemiBold, fontSize = 20.sp)
+                Text(
+                    text = "Your hotel rooms",
+                    fontFamily = PoppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp,vertical = 10.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                HotelRoomCard()
+                HotelRoomCard()
+                HotelRoomCard()
+                HotelRoomCard()
+                HotelRoomCard()
             }
         }
     }
 }
+
+
 
 @Preview(showBackground = true, showSystemUi = true, device = "spec:width=411dp,height=891dp")
 @Composable
---
+++ app/src/main/java/com/trekkstay/hotel/feature/hotel/presentation/fragments/hotel_room_card.kt
@@ -0,0 +1,113 @@
+package com.trekkstay.hotel.feature.hotel.presentation.fragments
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.hotel.R
+import com.trekkstay.hotel.ui.theme.PoppinsFontFamily
+import com.trekkstay.hotel.ui.theme.TrekkStayBlue
+
+@Composable
+fun HotelRoomCard() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White, shape = RoundedCornerShape(10.dp))
+            .border(2.dp, TrekkStayBlue, shape = RoundedCornerShape(10.dp))
+            .padding(15.dp)
+            .clickable {
+                //Navigate to edit room
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(135.dp)
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+            AsyncImage(
+                model = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/880ae3c6-0414-481b-aead-5968a48a560d/dfuky40-bbfeb6ac-8675-4cf7-a0b0-4bdbbc0299d4.png/v1/fill/w_512,h_512,q_80,strp/a_i__art___pretty_girl_by_draxionmufara_dfuky40-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTEyIiwicGF0aCI6IlwvZlwvODgwYWUzYzYtMDQxNC00ODFiLWFlYWQtNTk2OGE0OGE1NjBkXC9kZnVreTQwLWJiZmViNmFjLTg2NzUtNGNmNy1hMGIwLTRiZGJiYzAyOTlkNC5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.CYceDA6vX83Rkq_rhdV3Q4ObSpHylffYnDMvoY6i2x8",
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(135.dp)
+            )
+        }
+        Text(
+            "Superior Double or Twin Room",
+            fontFamily = PoppinsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp
+        )
+        RoomInfoRow(
+            icon = ImageVector.vectorResource(R.drawable.money_circle_ico),
+            label = "Original price",
+            value = "120.0"
+        )
+        RoomInfoRow(
+            icon = ImageVector.vectorResource(R.drawable.discount_ico),
+            label = "Discount rate",
+            value = "10%"
+        )
+        RoomInfoRow(
+            icon = ImageVector.vectorResource(R.drawable.box_ico),
+            label = "Quantity",
+            value = "4"
+        )
+    }
+}
+
+@Composable
+private fun RoomInfoRow(
+    icon: ImageVector,
+    label: String,
+    value: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = TrekkStayBlue
+        )
+        Text(
+            buildAnnotatedString {
+                append("$label: ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                    append(value)
+                }
+            },
+            fontFamily = PoppinsFontFamily,
+            fontSize = 16.sp,
+        )
+    }
+}
