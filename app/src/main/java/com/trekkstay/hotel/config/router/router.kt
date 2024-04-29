4604d1aa8d0be61985c1b14df7150b2b269b66e3
Bao Pham Hong Gia (Edward) <phamhonggiabaoqn0904@gmail.com>
2024-04-29 08:54:05 +0700

    chore: binding data

--- app/src/main/java/com/trekkstay/hotel/config/router/router.kt
+++ app/src/main/java/com/trekkstay/hotel/config/router/router.kt
@@ -34,6 +34,7 @@ import com.trekkstay.hotel.feature.hotel.presentation.activities.HotelHomeScreen
 import com.trekkstay.hotel.feature.hotel.presentation.activities.HotelRoomManageScreen
 import com.trekkstay.hotel.feature.hotel.presentation.activities.RoomDetailScreen
 import com.trekkstay.hotel.feature.hotel.presentation.activities.SearchEngineScreen
+import com.trekkstay.hotel.feature.hotel.presentation.activities.UpdateHotelScreen
 import com.trekkstay.hotel.feature.hotel.presentation.states.hotel.HotelViewModel
 import com.trekkstay.hotel.feature.hotel.presentation.states.location.LocationViewModel
 import com.trekkstay.hotel.feature.hotel.presentation.states.media.MediaViewModel
@@ -189,7 +190,7 @@ fun HotelRouter(empAuthViewModel: EmpAuthViewModel,hotelViewModel: HotelViewMode
             HotelProfileScreen(navController)
         }
         composable("hotel_create"){
-            CreateHotelScreen(hotelViewModel,locationViewModel, mediaViewModel,navController)
+            UpdateHotelScreen(hotelViewModel,locationViewModel, mediaViewModel,navController)
         }
         composable(route = "hotel_detail/{hotelId}") { backStackEntry ->
 
